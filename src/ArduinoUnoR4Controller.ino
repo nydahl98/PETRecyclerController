a3193476ba75ef4d8206d74363f7df063cada849
nydahl98 <nydahl98@gmail.com>
2023-08-27 02:35:53 +0200

    update

--- src/ArduinoUnoR4Controller.ino
+++ src/ArduinoUnoR4Controller.ino
@@ -65,7 +65,7 @@ int motDelayTime = 100;
 void setup() 
 {
   //Initialize serial communication
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   //SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
